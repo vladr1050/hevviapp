6768489026d54d449a3242f715c9d4eb52843253
Praesyx <steve.jsfolio@gmail.com>
2026-03-03 20:03:46 +0100

    order controller page updated

--- src/Security/UserAuthenticationEntryPoint.php
+++ src/Security/UserAuthenticationEntryPoint.php
@@ -4,13 +4,17 @@ namespace App\Security;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
 class UserAuthenticationEntryPoint implements AuthenticationEntryPointInterface
 {
+    public function __construct(protected readonly RouterInterface $router)
+    {
+    }
     public function start(Request $request, ?AuthenticationException $authException = null): RedirectResponse
     {
-        return new RedirectResponse('/');
+        return new RedirectResponse($this->router->generate('public_login'));
     }
 }
